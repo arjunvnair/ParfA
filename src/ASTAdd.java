772a6c8f249595a35c93c4fdb56370c4a0978922
Arjun Nair <arjunvnair@hotmail.com>
2017-07-05 21:43:53 -0500

    Create ASTAdd.java

---
+++ src/ASTAdd.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class ASTAdd extends SimpleNode 
+{
+	public ASTAdd(int id) 
+    {
+		super(id);
+    }
+
+	public ASTAdd(ParfA p, int id) 
+    {
+		super(p, id);
+    }
+  
+	@Override
+	public void interpret()
+	{
+		jjtGetChild(0).interpret();
+	    jjtGetChild(1).interpret();
+	    try
+	    {
+	    	if(ParfANode.stack[ParfANode.p - 1] instanceof String)
+	    		ParfANode.stack[--ParfANode.p] = new String(ParfANode.stack[ParfANode.p].toString() + ParfANode.stack[ParfANode.p + 1].toString());
+	    	else if(ParfANode.stack[ParfANode.p - 1] instanceof ArrayList && ParfANode.stack[ParfANode.p] instanceof ArrayList)
+	    	{
+	    		ArrayList<Object> list = new ArrayList<Object>();
+		    	list.addAll((ArrayList<Object>) ParfANode.stack[ParfANode.p - 1]);
+		    	list.addAll((ArrayList<Object>) ParfANode.stack[ParfANode.p]);
+		    	ParfANode.stack[--ParfANode.p] = new ArrayList<Object>(list);
+	    	}
+	    	else
+	    		ParfANode.stack[--ParfANode.p] = new Double(((Double) ParfANode.stack[ParfANode.p]).doubleValue() + ((Double) ParfANode.stack[ParfANode.p + 1]).doubleValue());
+	    }
+	    catch(ClassCastException e)
+	    {
+	    	throw new IllegalStateException();
+	    }
+  }
+}
